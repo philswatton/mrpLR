d2e2ac4520a172801f8c492e6d4a22bad8093e3d
Philip J. Swatton <56413126+philipjswatton@users.noreply.github.com>
2022-02-20 17:47:14 +0000

    init commit

---
+++ 04_mrp.R
@@ -0,0 +1,67 @@
+# MRP LR ----
+# Phil Swatton
+# University of Essex
+# File 04: Run MRP model
+
+
+## Packages
+library(tidyverse)
+library(lme4)
+
+
+## Data
+bes <- readRDS("data/bes2.rds")
+frame <- readRDS("data/frame.rds")
+marginals <- readRDS("data/marginals.rds")
+extras <- readRDS("data/extras.rds") %>%
+  mutate(across(matches("17"), ~ case_when(is.na(.x) ~ 0,
+                                           T ~ .x)))
+
+
+
+
+# Regression ----
+
+
+## Formula ----
+model <- lrscale ~ (1|gender) + (1|age) + (1|nssec) + (1|qualifications) + (1|ethnicity) + (1|owns) +
+  (1|constituency) +
+  perRemain + Con17 + SNP17 + Green17 + LD17 + PC17 + Other17 +
+  density
+
+
+## Run regression ----
+result <- lmer(model, bes)
+
+# Save the output
+saveRDS(result, "results/model.rds")
+
+
+
+# Poststratification ----
+
+## Build frame ----
+frame <- left_join(frame, extras)
+
+
+## Generate predictions ----
+pred <- predict(result, frame, allow.new.levels = T)
+saveRDS(pred, "results/pred.RDS")
+frame$pred <- pred
+
+
+## Poststratify
+post <- frame %>%
+  mutate(wpred = pred * n) %>%
+  group_by(constituency) %>%
+  summarise(lrscale = sum(wpred)/sum(n))
+saveRDS(post, "results/post.rds")
+
+
+
+
+
+
+
+
+
